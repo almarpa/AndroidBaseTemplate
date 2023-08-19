6810ef1215489b1619c51659bde428f7bac50b62
almarpa <alex_marco7@outlook.es>
2023-08-20 01:10:29 +0200

    feat(resources): add styles, resources and strings

---
+++ app/src/main/java/com/example/androidbasetemplate/ui/theme/Shape.kt
@@ -0,0 +1,11 @@
+package com.example.androidbasetemplate.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Shapes
+import androidx.compose.ui.unit.dp
+
+val TemplateShapes = Shapes(
+    small = RoundedCornerShape(4.dp),
+    medium = RoundedCornerShape(4.dp),
+    large = RoundedCornerShape(8.dp),
+)
