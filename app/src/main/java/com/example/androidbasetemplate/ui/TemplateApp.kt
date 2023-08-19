d51132cdc7f74e312a69a0ed7037985fdd7adf27
almarpa <alex_marco7@outlook.es>
2023-08-20 01:14:49 +0200

    feat(template-drawer): refactor code

--- app/src/main/java/com/example/androidbasetemplate/ui/TemplateApp.kt
+++ app/src/main/java/com/example/androidbasetemplate/ui/TemplateApp.kt
@@ -10,36 +10,29 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
-import com.example.androidbasetemplate.ui.theme.TemplateTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TemplateApp() {
-    TemplateTheme {
-        val navController = rememberNavController()
-        val navigationActions = remember(navController) {
-            TemplateNavigationActions(navController)
-        }
+    val navController = rememberNavController()
+    val navigationActions = remember(navController) { TemplateNavigationActions(navController) }
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentRoute = navBackStackEntry?.destination?.route ?: TemplateDestinations.HOME_ROUTE
 
-        val navBackStackEntry by navController.currentBackStackEntryAsState()
-        val currentRoute =
-            navBackStackEntry?.destination?.route ?: TemplateDestinations.HOME_ROUTE
-
-        ModalNavigationDrawer(
-            drawerContent = {
-                AppDrawer(
-                    currentRoute = currentRoute,
-                    navigateToHome = navigationActions.navigateToHome,
-                    navigateToProductList = navigationActions.navigateToProductList,
-                )
-            },
-            drawerState = DrawerState(DrawerValue.Closed),
-        ) {
-            Row {
-                TemplateNavGraph(
-                    navController = navController,
-                )
-            }
+    ModalNavigationDrawer(
+        drawerContent = {
+            AppDrawer(
+                currentRoute = currentRoute,
+                navigateToHome = navigationActions.navigateToHome,
+                navigateToProductList = navigationActions.navigateToProductList,
+            )
+        },
+        drawerState = DrawerState(DrawerValue.Closed),
+    ) {
+        Row {
+            TemplateNavGraph(
+                navController = navController,
+            )
         }
     }
 }
