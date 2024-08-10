1391114cdbcc7a4a85006dcaf505862d8a02afa3
almarpa <alex_marco7@outlook.es>
2024-08-11 00:38:55 +0200

    feat(pokemon-details): refactor add member button

--- app/src/main/java/com/example/androidtemplateapp/ui/pokemondetails/PokemonDetailsScreen.kt
+++ app/src/main/java/com/example/androidtemplateapp/ui/pokemondetails/PokemonDetailsScreen.kt
@@ -1,20 +1,33 @@
 package com.example.androidtemplateapp.ui.pokemondetails
 
+import android.annotation.SuppressLint
+import android.content.res.Configuration
 import androidx.activity.compose.BackHandler
 import androidx.compose.animation.AnimatedVisibilityScope
 import androidx.compose.animation.ExperimentalSharedTransitionApi
 import androidx.compose.animation.SharedTransitionScope
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.PersonRemove
+import androidx.compose.material.icons.outlined.PersonAdd
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
@@ -35,10 +48,12 @@ import com.example.androidtemplateapp.entity.enums.AppTheme
 import com.example.androidtemplateapp.ui.common.mocks.getPokemonDetailsMock
 import com.example.androidtemplateapp.ui.common.mocks.getPokemonMock
 import com.example.androidtemplateapp.ui.common.preview.TemplatePreviewTheme
+import com.example.androidtemplateapp.ui.common.spacer.CustomSpacer
 import com.example.androidtemplateapp.ui.common.topappbar.DefaultTopAppBar
 import com.example.androidtemplateapp.ui.common.utils.isTablet
 import java.net.URLDecoder
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @OptIn(ExperimentalSharedTransitionApi::class)
 @Composable
 fun SharedTransitionScope.PokemonDetailsScreen(
@@ -50,17 +65,35 @@ fun SharedTransitionScope.PokemonDetailsScreen(
     onBackPressed: () -> Unit,
 ) {
     BackHandler { onBackPressed() }
+    Scaffold(
+        topBar = { DefaultTopAppBar(title = R.string.empty_string) { onBackPressed() } },
+    ) { paddingValues ->
+        PokemonDetailsContent(
+            userAppTheme = userAppTheme,
+            pokemon = pokemon,
+            pokemonDetails = pokemonDetails,
+            animatedVisibilityScope = animatedVisibilityScope,
+            onAddTeamMember = onAddTeamMember
+        )
+    }
+}
+
+@OptIn(ExperimentalSharedTransitionApi::class)
+@Composable
+private fun SharedTransitionScope.PokemonDetailsContent(
+    userAppTheme: AppTheme,
+    pokemon: Pokemon,
+    pokemonDetails: PokemonDetails?,
+    animatedVisibilityScope: AnimatedVisibilityScope,
+    onAddTeamMember: (Pokemon, Boolean) -> Unit,
+) {
     Box(
         modifier = Modifier
             .fillMaxSize()
             .background(getBackgroundColor(userAppTheme, pokemon.dominantColor))
-            .statusBarsPadding().systemBarsPadding(),
+            .statusBarsPadding()
+            .systemBarsPadding(),
     ) {
-        DefaultTopAppBar(
-            modifier = Modifier,
-            title = R.string.empty_string,
-            onBackPressed = { onBackPressed() }
-        )
         PokemonCard(
             modifier = Modifier
                 .fillMaxSize()
@@ -68,17 +101,64 @@ fun SharedTransitionScope.PokemonDetailsScreen(
                     top = 150.dp,
                     start = 16.dp,
                     end = 16.dp,
-                    bottom = 30.dp
+                    bottom = 16.dp
                 )
                 .shadow(10.dp, RoundedCornerShape(10.dp))
-                .clip(RoundedCornerShape(10.dp))
                 .background(MaterialTheme.colorScheme.surface)
                 .padding(horizontal = 16.dp),
             pokemon = pokemon,
             pokemonDetails = pokemonDetails,
-            onMemberClick = { isAdded -> onAddTeamMember(pokemon, isAdded) }
         )
         PokemonImageAnimation(animatedVisibilityScope, pokemon)
+        AddMemberButton(
+            modifier = Modifier
+                .align(Alignment.TopStart)
+                .padding(
+                    top = 166.dp,
+                    start = 32.dp,
+                ),
+            isMemberYet = pokemon.isTeamMember
+        ) { isAdded ->
+            onAddTeamMember(
+                pokemon,
+                isAdded
+            )
+        }
+    }
+}
+
+
+@Preview("Add Member Button")
+@Preview("Dark Add Member Button", uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+fun AddMemberButton(
+    modifier: Modifier = Modifier,
+    isMemberYet: Boolean = false,
+    onMemberClick: (Boolean) -> Unit = {},
+) {
+    Box(modifier = modifier) {
+        var isTeamMember by remember { mutableStateOf(isMemberYet) }
+        val memberIconScale by animateFloatAsState(
+            targetValue = if (isTeamMember) 1.2f else 1f,
+            label = "Member Button Scale"
+        )
+        IconButton(
+            modifier = Modifier
+                .size(if (isTablet()) 70.dp else 40.dp)
+                .aspectRatio(1f)
+                .scale(memberIconScale),
+            onClick = {
+                isTeamMember = !isTeamMember
+                onMemberClick(isTeamMember)
+            },
+        ) {
+            Icon(
+                modifier = Modifier.fillMaxSize(),
+                imageVector = if (isTeamMember) Icons.Filled.PersonRemove else Icons.Outlined.PersonAdd,
+                contentDescription = "Add member icon",
+                tint = MaterialTheme.colorScheme.primary,
+            )
+        }
     }
 }
 
@@ -87,7 +167,6 @@ fun PokemonCard(
     modifier: Modifier = Modifier,
     pokemon: Pokemon,
     pokemonDetails: PokemonDetails?,
-    onMemberClick: (Boolean) -> Unit,
 ) {
     val scrollState = rememberScrollState()
     Column(
@@ -95,14 +174,17 @@ fun PokemonCard(
         modifier = modifier
             .wrapContentHeight()
             .padding(
-                top = if (isTablet()) 230.dp else 50.dp,
+                top = if (isTablet()) 230.dp else 20.dp,
             )
             .verticalScroll(scrollState)
     ) {
         pokemonDetails?.let { pokemonDetailsNotNull ->
-            PokemonName(pokemon) { onMemberClick(it) }
+            PokemonName(pokemon)
+            CustomSpacer(height = 16)
             PokemonType(pokemonDetailsNotNull.types)
+            CustomSpacer(height = 8)
             PokemonMeasures(pokemonDetailsNotNull.weight, pokemonDetailsNotNull.height)
+            CustomSpacer(height = 8)
             PokemonStats(pokemonDetailsNotNull)
         }
     }
@@ -175,7 +257,6 @@ fun PokemonCardPreview() {
         PokemonCard(
             pokemonDetails = getPokemonDetailsMock(),
             pokemon = getPokemonMock(),
-            onMemberClick = { }
         )
     }
 }
