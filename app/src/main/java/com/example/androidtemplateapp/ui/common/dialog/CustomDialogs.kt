892ad2534dec33e6280e7bb7bf1224d6b43ea02c
almarpa <alex_marco7@outlook.es>
2024-09-08 21:57:16 +0200

    feat(custom-dialogs): refactor simple dialog

--- app/src/main/java/com/example/androidtemplateapp/ui/common/dialog/CustomDialogs.kt
+++ app/src/main/java/com/example/androidtemplateapp/ui/common/dialog/CustomDialogs.kt
@@ -8,8 +8,13 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -21,27 +26,27 @@ import com.example.androidtemplateapp.R
 @Composable
 fun Context.SimpleAlertDialog(
     show: Boolean = true,
-    title: Int = R.string.empty_string,
-    description: Int = R.string.empty_string,
+    title: String = stringResource(R.string.app_error_title),
+    description: String = stringResource(R.string.empty_string),
     onConfirm: () -> Unit = {},
     confirmText: Int = R.string.common_accept,
-    onCancel: () -> Unit = {},
-    cancelText: Int = R.string.common_cancel,
     onDismissRequest: () -> Unit = {},
 ) {
-    if (show) {
+    var isActive by rememberSaveable { mutableStateOf(show) }
+
+    if (isActive) {
         AlertDialog(
             onDismissRequest = { onDismissRequest() },
-            title = { Text(text = getString(title)) },
-            text = { Text(text = getString(description)) },
+            title = { Text(text = title) },
+            text = { Text(text = description) },
             confirmButton = {
-                TextButton(onClick = { onConfirm() }) {
+                TextButton(
+                    onClick = {
+                        isActive = false
+                        onConfirm()
+                    }
+                ) {
                     Text(text = getString(confirmText))
-                }
-            },
-            dismissButton = {
-                TextButton(onClick = { onCancel() }) {
-                    Text(text = getString(cancelText))
                 }
             },
             properties = DialogProperties(
@@ -115,8 +120,8 @@ fun Context.CustomDialog(
 @Composable
 fun SimpleAlertDialogPreview() {
     LocalContext.current.SimpleAlertDialog(
-        title = R.string.search_title,
-        description = R.string.error_getting_pokemon_list,
+        title = stringResource(R.string.search_title),
+        description = stringResource(R.string.error_getting_pokemon_list),
     )
 }
 
