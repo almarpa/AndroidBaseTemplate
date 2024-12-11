b4234da7a5a0b9509eb5284f7c67ea25a5c46e60
almarpa <alex_marco7@outlook.es>
2024-12-11 07:55:45 +0100

    fix(pokemon-api): remove suspend

--- app/src/main/java/com/example/androidtemplateapp/data/db/ws/api/PokemonApi.kt
+++ app/src/main/java/com/example/androidtemplateapp/data/db/ws/api/PokemonApi.kt
@@ -16,5 +16,5 @@ interface PokemonApi {
     ): PokemonResultResponse
 
     @GET("api/v2/pokemon/{pokemonID}")
-    suspend fun getPokemon(@Path("pokemonID") pokemonId: Int): Call<PokemonDetailsResponse>
+    fun getPokemon(@Path("pokemonID") pokemonId: Int): Call<PokemonDetailsResponse>
 }
